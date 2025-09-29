7aa944070a10812e37134674c85f548ebc61e9c5
peep12ng <peep12ng@gmail.com>
2025-09-29 16:11:39 +0900

    프론트엔드 UI 템플릿 구현

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! Flask 서버가 실행 중입니다."
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
